a3b52cfdd3036e7058673b20d81a55d4fdf99757
Vidit Khandelwal <vidit@gkmit.co>
2025-12-03 22:38:10 +0530

    feat: comments added, merge

--- mergec.cpp
+++ mergec.cpp
@@ -1,1 +1,11 @@
-// zxcv
+// qwertyuioiuytrewedxc mjhgfds
+// wertyuhgfdxsawertyujhbvcs
+// adsfghrjytu564yt4rwfdscdvbfgn
+// __GCC_HAVE_DWARF2_CFI_ASMfgrhetrjytkuymgndfb
+// dvscvdbfn
+// mynfbs
+// dvagrwey5
+// u645y4
+// er5htbf
+// vzxasdeGRE%$#2__DBL_HAS_QUIET_NAN__ewhrsfn
+// bcz x
